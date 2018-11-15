3ee0a7ad4ef6b838fdd570009e6b4b0503a60f3e
Jakob Eriksson <jakob@uic.edu>
2018-11-14 23:50:00 -0600

    added dummies for better making

---
+++ fixed.c
@@ -0,0 +1,27 @@
+#include"min.h"
+
+int arraymin(int* array, int size) {
+	int min=array[0];
+	for(unsigned i=1;i<size;i++) {
+		if(array[i]<min) {
+			min=array[i];
+		}
+	}
+	return min;
+}
+
+int arraymin64(int* array) {
+	return arraymin(array,64);
+}
+
+int minindex(int* array, int size) {
+	int min=array[0];
+	int index=0;
+	for(unsigned i=1;i<size;i++) {
+		if(array[i]<min) {
+			min=array[i];
+			index=i;
+		}
+	}
+	return index;
+}
